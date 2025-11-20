b1bdd5b3f0bade652db7ed51e861904629819bfd
unknown <niloy9896@gmail.com>
2025-11-20 20:00:02 +0600

    Change name of data file

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,7 +8,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
-import { items } from "@/app/data/page";
+import { items } from "@/app/data/data";
 export function AppSidebar() {
   return (
     <Sidebar>
