c2a9e107b656362d58d78b6aa22254fe494bf9a1
unknown <niloy9896@gmail.com>
2025-12-05 19:02:04 +0600

    Add current Doctor name on NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,24 +3,31 @@ import { usePathname } from "next/navigation";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { getTitle } from "@/lib/utils";
 import Link from "next/link";
+import { useDoctorsStore } from "@/hooks/use-DoctorsStore";
 
 const NavBar = () => {
   const pathname = usePathname();
   const pageHeader = getTitle(pathname);
+  const { doctors, currentDoctorId } = useDoctorsStore();
+  const currentDoctor = doctors.find((d) => d.id === currentDoctorId);
   return (
     <div className="flex justify-between items-center p-4 border-b">
       <div className="text-xl font-semibold">{pageHeader}</div>
       <div className="flex items-center space-x-2">
         <Link href="/doctor-profile">
-          <div>
-            <h1 className="font-semibold">Nazmus Sakib</h1>
-            <h1 className="text-sm text-muted-foreground text-right">Admin</h1>
+          <div className="text-right">
+            <h1 className="font-semibold">
+              {currentDoctor?.name || "No Doctor Selected"}
+            </h1>
+            <h1 className="text-sm text-muted-foreground">Doctor</h1>
           </div>
         </Link>
         <Avatar>
           <Link href="/doctor-profile">
             <AvatarImage src="https://github.com/shadcn.png" />
-            <AvatarFallback>User</AvatarFallback>
+            <AvatarFallback>
+              {currentDoctor?.name?.charAt(0) || "D"}
+            </AvatarFallback>
           </Link>
         </Avatar>
       </div>
